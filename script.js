16bedbe34f4232a47ffb63c725d60fdf84ef9a5e
ssaldana <ssaldana.rangel@gmail.com>
2024-06-14 19:02:26 -0600

    Made it usable

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const message = document.querySelector('#cipher_input');
 const cipherButton = document.querySelector('#cipher_button');
+const cipherOutput = document.querySelector('#cipher_output');
+cipherButton.addEventListener('click', triggerCipher);
 let rounds = 3;
 
 let mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','Á', 'É', 'Í', 'Ó', 'Ú'];
 let minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','á', 'é', 'í', 'ó', 'ú'];
+
+function triggerCipher () {
+    let outputText = document.createTextNode(cipher(message.value, rounds).toString());
+    cipherOutput.appendChild(outputText);
+}
 
 function cipher (text, key) {
     let cipheredText = '';
